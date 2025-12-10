78fa47e2c0fb19640fce84804f36520ce83751bd
Gabriel Barbosa <gabrieldesousabarbosa40@gmail.com>
2025-12-10 00:02:48 -0300

    Atualização da tela de cadastro e ajustes no build.xml

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+
+import view.TelaCadastro;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,7 @@
  * @author gabriel
  */
 public class App {
-    
+    public static void main(String[] args){
+            new TelaCadastro().setVisible(true);
+    }
 }
